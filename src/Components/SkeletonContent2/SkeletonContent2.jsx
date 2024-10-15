edd5a4e9a345cbab4d4600a68873419dec101745
mohamadkzd <kazemzademohammad4@gmail.com>
2024-10-15 12:54:00 +0330

    skeletonContent2 update

--- src/Components/SkeletonContent2/SkeletonContent2.jsx
+++ src/Components/SkeletonContent2/SkeletonContent2.jsx
@@ -3,14 +3,18 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 const SkeletonContent2 = () => {
   return (
-    <div className="max-w-[1200px] mx-auto">
-      <SkeletonTheme baseColor="#202020" highlightColor="#444" height={"170px"} >
-         <h1>
-          <Skeleton count={1} height={"70px"} />
-        </h1>
-        <p className="flex w-[20%]">
-          <Skeleton count={5} borderRadius={"100%"} width={"15%"} inline={true} containerClassName="flex-1" />
-        </p>
+    <div className="max-w-[1300px] mx-auto">
+      <SkeletonTheme baseColor="#d3d3d3" highlightColor="#9b9999" height={"500px"} >
+         <header>
+          <Skeleton count={1}  height={"170px"} />
+          
+        </header>
+        <section className="">
+          <Skeleton count={1}   width={"100%"} containerClassName="flex-1" />
+        </section>
+        <footer className="">
+          <Skeleton count={1}   width={"100%"} height={"170px"} containerClassName="flex-1" />
+        </footer>
       </SkeletonTheme>
     </div>
   );
